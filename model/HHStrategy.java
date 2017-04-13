68072c736f3e84995e009ba79989d6a90cd6981d
HasanKarilov <hkarilov@gmail.com>
2017-04-13 19:32:28 +0600

    URL_FORMAT 04

--- model/HHStrategy.java
+++ model/HHStrategy.java
@@ -4,4 +4,5 @@ package com.javarush.task.task28.task2810.model;
  * Created by hanaria on 4/13/17.
  */
 public class HHStrategy implements Strategy {
+    private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
 }
