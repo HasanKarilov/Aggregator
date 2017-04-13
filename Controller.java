8a11aae15f926229c4e2ca6fb055c931dbd04a29
HasanKarilov <hkarilov@gmail.com>
2017-04-13 19:43:46 +0600

    Aggregator 05'

--- Controller.java
+++ Controller.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -21,5 +24,19 @@ public class Controller {
         return "Controller{" +
                 "providers=" + Arrays.toString(providers) +
                 '}';
+    }
+
+    public void scan() {
+        ArrayList<Vacancy> vacs = new ArrayList<>();
+        try{
+            for (Provider provider:providers){
+                for (Vacancy vacancy:provider.getJavaVacancies("SOME TEXT")){
+                    vacs.add(vacancy);
+                }
+            }
+        }catch(NullPointerException e){/*NOP*/} catch (IOException e) {
+
+        }
+        System.out.println(vacs.size());
     }
 }
--- model/HHStrategy.java
+++ model/HHStrategy.java
@@ -1,8 +1,18 @@
 package com.javarush.task.task28.task2810.model;
 
+import com.javarush.task.task28.task2810.vo.Vacancy;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by hanaria on 4/13/17.
  */
 public class HHStrategy implements Strategy {
     private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
+
+    @Override
+    public List<Vacancy> getVacancies(String searchString) throws IOException {
+        return null;
+    }
 }
