068ca07ee7db1ccd7856307adf1dc28b9d03829d
HasanKarilov <hkarilov@gmail.com>
2017-04-18 16:30:06 +0600

    First test on MVC 12

--- view/HtmlView.java
+++ view/HtmlView.java
@@ -3,6 +3,7 @@ package com.javarush.task.task28.task2810.view;
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -12,11 +13,16 @@ public class HtmlView implements View {
     private Controller controller;
     @Override
     public void update(List<Vacancy> vacancies) {
-
+        System.out.println(vacancies.size());
     }
 
     @Override
     public void setController(Controller controller) {
         this.controller = controller;
     }
+
+
+   public void userCitySelectEmulationMethod() throws IOException {
+       controller.onCitySelect("Odessa");
+   }
 }
