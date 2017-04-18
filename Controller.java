7579f7428303ea83f95456ef7ed878615f5e0de2
HasanKarilov <hkarilov@gmail.com>
2017-04-18 16:08:28 +0600

    Controller 11

--- Controller.java
+++ Controller.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task28.task2810;
 
+import com.javarush.task.task28.task2810.model.Model;
 import com.javarush.task.task28.task2810.model.Provider;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
@@ -11,33 +12,17 @@ import java.util.Arrays;
  * Created by hanaria on 4/13/17.
  */
 public class Controller {
-    private Provider[] providers;
-    public Controller(Provider... providers) {
-        if (providers==null||providers.length==0){
+    private Model model;
+
+    public Controller(Model model) {
+        if (model == null){
             throw new IllegalArgumentException();
         }
-        this.providers = providers;
+        this.model = model;
     }
 
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
-    }
-
-    public void scan() {
-        ArrayList<Vacancy> vacs = new ArrayList<>();
-        try{
-            for (Provider provider:providers){
-                for (Vacancy vacancy:provider.getJavaVacancies("Odessa")){
-                    vacs.add(vacancy);
-                }
-            }
-        }catch(NullPointerException e){/*NOP*/} catch (IOException e) {
-
-        }
-        System.out.println(vacs.size());
-
+    public void onCitySelect(String cityName) throws IOException
+    {
+        model.selectCity(cityName);
     }
 }
